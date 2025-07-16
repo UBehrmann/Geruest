3476efc5584d6b5d6c674759fbad918f670b9467
Urs Behrmann <urs.behrmann@outlook.com>
2025-07-16 10:34:44 +0200

    Refactor Handler.cpp: reorder includes, improve formatting, and enhance readability

--- src/handler/Handler.cpp
+++ src/handler/Handler.cpp
@@ -8,20 +8,22 @@
  */
 
 #include "Handler.hpp"
+
+#include <algorithm>
+#include <climits>
+
+#include "builders/CSSBuilder.hpp"
 #include "builders/ContentBuilder.hpp"
 #include "builders/HTMLBuilder.hpp"
 #include "builders/JSBuilder.hpp"
-#include "builders/CSSBuilder.hpp"
-
-#include <climits>
-#include <algorithm>
 
 #ifdef _WIN32
-Handler::Handler(SOCKET socket, std::string IP, ServerData* serverData)
+Handler::Handler(SOCKET socket, std::string IP, ServerData *serverData)
 #else
-Handler::Handler(int socket, std::string IP, ServerData* serverData)
+Handler::Handler(int socket, std::string IP, ServerData *serverData)
 #endif
-    : clientSocket(socket), IP(std::move(IP)), serverData(serverData) {}
+    : clientSocket(socket), IP(std::move(IP)), serverData(serverData) {
+}
 
 Handler::~Handler() {
 #ifdef _WIN32
@@ -34,7 +36,7 @@ Handler::~Handler() {
     //    logger.log("Client socket closed.");
 }
 
-bool Handler::readSocket(char* bufferToUse, size_t size) {
+bool Handler::readSocket(char *bufferToUse, size_t size) {
     // test if size is bigger than int
     if (size > INT_MAX) {
         sendToLoggerError("Size of buffer is too big.");
@@ -60,7 +62,7 @@ bool Handler::readSocket(char* bufferToUse, size_t size) {
             sendToLogger("Timeout for receiving data.");
             idling++;
             if (idling > 2) {
-					 std::cout << "Client is idling too long. Closing connection." << std::endl;
+                std::cout << "Client is idling too long. Closing connection." << std::endl;
             }
         }
         sendToLogger("Error reading from socket.");
@@ -71,7 +73,7 @@ bool Handler::readSocket(char* bufferToUse, size_t size) {
     return false;
 }
 
-bool Handler::sendSocket(const char* bufferToSend, size_t size) const {
+bool Handler::sendSocket(const char *bufferToSend, size_t size) const {
     char bufferToSocket[BUFFER_SIZE];
 
     size_t startPos = 0;
@@ -91,20 +93,20 @@ bool Handler::sendSocket(const char* bufferToSend, size_t size) const {
     return true;
 }
 
-void Handler::sendToLogger(const std::string& message) const {
-	 std::cout << "Log: " << message << " from " << IP << std::endl;
+void Handler::sendToLogger(const std::string &message) const {
+    std::cout << "Log: " << message << " from " << IP << std::endl;
 }
-void Handler::sendToLoggerPages(const std::string& message) const {
-	std::cout << "Page Log: " << message << " from " << IP << std::endl;
+void Handler::sendToLoggerPages(const std::string &message) const {
+    std::cout << "Page Log: " << message << " from " << IP << std::endl;
 }
-void Handler::sendToLoggerAPI(const std::string& message) const {
-	std::cout << "API Log: " << message << " from " << IP << std::endl;
+void Handler::sendToLoggerAPI(const std::string &message) const {
+    std::cout << "API Log: " << message << " from " << IP << std::endl;
 }
-void Handler::sendToLoggerUser(const std::string& message) const {
-	std::cout << "User Log: " << message << " from " << IP << std::endl;
+void Handler::sendToLoggerUser(const std::string &message) const {
+    std::cout << "User Log: " << message << " from " << IP << std::endl;
 }
-void Handler::sendToLoggerError(const std::string& message) const {
-	std::cerr << "Error Log: " << message << " from " << IP << std::endl;
+void Handler::sendToLoggerError(const std::string &message) const {
+    std::cerr << "Error Log: " << message << " from " << IP << std::endl;
 }
 
 void Handler::run() {
@@ -133,7 +135,7 @@ void Handler::run() {
     }
 }
 
-void Handler::handleRequest(HTTPRequest* request) {
+void Handler::handleRequest(HTTPRequest *request) {
     if (request == nullptr) {
         sendToLoggerError("HTTPRequest is null.");
         std::string header = buildInternalServerErrorHeader();
@@ -151,7 +153,6 @@ void Handler::handleRequest(HTTPRequest* request) {
         return;
 
     } else {
-
         std::string path = request->getPathString();
 
         // Here html logic should be added
@@ -159,8 +160,11 @@ void Handler::handleRequest(HTTPRequest* request) {
         std::string content_type = getContentType(extension);
         std::string contentPath = buildPath(path, extension, request);
 
-        sendFile(content_type, contentPath, request);
+        std::cout << "Content path: " << contentPath << std::endl;
+		std::cout << "Content type: " << content_type << std::endl;
+		std::cout << "Extension: " << extension << std::endl;
 
+        sendFile(content_type, contentPath, request);
     }
 
     sendToLoggerPages("Not found: " + request->getPathString());
@@ -177,16 +181,13 @@ void Handler::handleRequest(HTTPRequest* request) {
  * @param contentType
  * @param content
  */
-void Handler::sendResponse(const std::string &status, const std::string &contentType, const std::string &content) const {
+void Handler::sendResponse(const std::string &status, const std::string &contentType,
+                           const std::string &content) const {
+    std::string response = buildHeader(status, contentType, std::to_string(content.size()));
 
-	std::string response = buildHeader(status,
-												  contentType,
-												  std::to_string(content.size())
-	);
+    response += content;
 
-	response += content;
-
-	sendSocket((char *) response.c_str(), response.size());
+    sendSocket((char *)response.c_str(), response.size());
 }
 
 /**
@@ -194,118 +195,108 @@ void Handler::sendResponse(const std::string &status, const std::string &content
  * @param contentType
  * @param contentPath
  */
-void Handler::sendFile(const std::string &contentType, const std::string &contentPath,
-							  HTTPRequest* httpRequest) const {
+void Handler::sendFile(const std::string &contentType, const std::string &contentPath, HTTPRequest *httpRequest) const {
+    char bufferToSend[BUFFER_SIZE];
 
-	char bufferToSend[BUFFER_SIZE];
+    // sendToLoggerPages("Sending file: " + contentPath);
 
-	// sendToLoggerPages("Sending file: " + contentPath);
+    if (contentType == "text/html" || contentType == "text/javascript" || contentType == "text/css") {
+        ContentBuilder *contentBuilder = nullptr;
 
-	if (contentType == "text/html" || contentType == "text/javascript" || contentType == "text/css") {
+        if (contentType == "text/html") {
+            //			sendToLoggerPages("GET: " + path);
 
-		ContentBuilder *contentBuilder = nullptr;
+            int access = 1;  // hasAccess(requestStream.str(), httpRequest->getPathString(),
+            // serverData->getRoot());
 
-		if (contentType == "text/html") {
+            if (access == 0) {
+                std::string header = buildAuthHeader();
+                sendSocket(header.c_str(), header.size());
+                return;
+            }
 
-//			sendToLoggerPages("GET: " + path);
+            if (access == -1) {
+                std::string header = buildForbiddenHeader();
+                sendSocket(header.c_str(), header.size());
+                return;
+            }
 
-			int access = 1 ; // hasAccess(requestStream.str(), httpRequest->getPathString(),
-			// serverData->getRoot());
+            contentBuilder = new HtmlBuilder(contentPath, serverData->getRoot());
 
-			if (access == 0) {
-				std::string header = buildAuthHeader();
-				sendSocket(header.c_str(), header.size());
-				return;
-			}
+        } else if (contentType == "text/javascript") {
+            contentBuilder = new JSBuilder(contentPath, serverData->getRoot());
+        } else if (contentType == "text/css") {
+            contentBuilder = new CSSBuilder(contentPath, serverData->getRoot());
+        }
 
-			if (access == -1) {
-				std::string header = buildForbiddenHeader();
-				sendSocket(header.c_str(), header.size());
-				return;
-			}
+        // Check if builder was created
+        if (!contentBuilder) {
+            // sendToLogger("File not found: " + contentPath);
+            sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
+            return;
+        }
 
-			contentBuilder = new HtmlBuilder(contentPath, serverData->getRoot());
+        // Check if content is empty
+        if (contentBuilder->size() == 0) {
+            sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
+            return;
+        }
 
-		} else if (contentType == "text/javascript") {
-			contentBuilder = new JSBuilder(contentPath, serverData->getRoot());
-		} else if (contentType == "text/css") {
-			contentBuilder = new CSSBuilder(contentPath, serverData->getRoot());
-		}
+        std::string header = buildHeader("200 OK", contentType, contentBuilder->sizeString());
 
-		// Check if builder was created
-		if (!contentBuilder) {
-			// sendToLogger("File not found: " + contentPath);
-			sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
-			return;
-		}
+        std::string bufferToSocket = header + contentBuilder->file();
 
-		// Check if content is empty
-		if (contentBuilder->size() == 0) {
-			sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
-			return;
-		}
+        sendSocket(bufferToSocket.c_str(), bufferToSocket.size());
 
-		std::string header = buildHeader("200 OK",
-										 contentType,
-										 contentBuilder->sizeString()
-		);
+        delete contentBuilder;
 
-		std::string bufferToSocket = header + contentBuilder->file();
+    } else {
+        // Open file
+        std::ifstream file(contentPath, std::ios::binary);
 
-		sendSocket(bufferToSocket.c_str(), bufferToSocket.size());
+        if (!file.is_open()) {
+            sendToLogger("File not found: " + contentPath);
+            sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
+            return;
+        }
 
-		delete contentBuilder;
+        // Get file size
+        file.seekg(0, std::ios::end);
+        size_t fileSize = static_cast<size_t>(file.tellg());
+        file.seekg(0, std::ios::beg);
 
-	} else {
-		// Open file
-		std::ifstream file(contentPath, std::ios::binary);
+        // sendToLogger("File size: " + std::to_string(fileSize));
 
-		if (!file.is_open()) {
-			sendToLogger("File not found: " + contentPath);
-			sendResponse("404 Not Found", "text/html", "<html><body><h1>404 Not Found</h1></body></html>");
-			return;
-		}
+        std::string response = buildHeader("200 OK", contentType, std::to_string(fileSize));
 
-		// Get file size
-		file.seekg(0, std::ios::end);
-		size_t fileSize = static_cast<size_t>(file.tellg());
-		file.seekg(0, std::ios::beg);
+        // Send response
+        sendSocket((char *)response.c_str(), response.size());
 
-		// sendToLogger("File size: " + std::to_string(fileSize));
+        while (!file.eof()) {
+            file.read(bufferToSend, BUFFER_SIZE);
+            sendSocket(bufferToSend, static_cast<size_t>(file.gcount()));
+        }
 
-		std::string response = buildHeader("200 OK",
-										   contentType,
-										   std::to_string(fileSize)
-		);
-
-		// Send response
-		sendSocket((char *) response.c_str(), response.size());
-
-		while (!file.eof()) {
-			file.read(bufferToSend, BUFFER_SIZE);
-			sendSocket(bufferToSend, static_cast<size_t>(file.gcount()));
-		}
-
-		file.close();
-	}
+        file.close();
+    }
 }
 
 std::string Handler::getExtension(const std::string &path) {
-	// Check if path is for a file or a page
-	if (path.find('.') == std::string::npos) {
-		return "html";
-	} else {
-		// Get the extension of the file
-		return path.substr(path.find('.') + 1);
-	}
+    // Check if path is for a file or a page
+    if (path.find('.') == std::string::npos) {
+        return "html";
+    } else {
+        // Get the extension of the file
+        return path.substr(path.find('.') + 1);
+    }
 }
 
-//void Handler::removeSearchParameters() {
+// void Handler::removeSearchParameters() {
 //
 //	if (path.find('?') != std::string::npos) {
 //		path = path.substr(0, path.find('?'));
 //	}
-//}
+// }
 
 /**
  * Check if the path starts with a language prefix
@@ -313,48 +304,24 @@ std::string Handler::getExtension(const std::string &path) {
  * @return true if it starts with a language prefix
  */
 bool startsWithLangPrefix(const std::string &str) {
-	return str.size() >= 4 &&
-			 str[0] == '/' &&
-			 std::isalpha(str[1]) &&
-			 std::isalpha(str[2]) &&
-			 str[3] == '/';
+    return str.size() >= 4 && str[0] == '/' && std::isalpha(str[1]) && std::isalpha(str[2]) && str[3] == '/';
 }
 
-std::string Handler::buildPath(std::string &pathReceived, const std::string &Extension, HTTPRequest* httpRequest)
-{
+std::string Handler::buildPath(std::string &pathReceived, const std::string &Extension, HTTPRequest *httpRequest) {
+    std::map<std::string, std::string> contentRoot = {
+        {"html", "/html"},         {"htm", "/html"},           {"css", "/assets/css"},    {"js", "/assets/js"},
+        {"jpg", "/assets/images"}, {"jpeg", "/assets/images"}, {"png", "/assets/images"}, {"gif", "/assets/images"},
+        {"svg", "/assets/images"}, {"ico", "/assets/images"},  {"JSON", "/assets/JSONs"}, {"pdf", "/assets/docs"},
+        {"zip", "/assets/docs"},   {"mp3", "/assets/audio"},   {"mp4", "/assets/video"},  {"xml", "/assets/docs"},
+        {"csv", "/assets/docs"},   {"txt", "/assets/docs"}};
 
-	std::map<std::string, std::string> contentRoot = {
-		{"html", "/html"},
-		{"htm",  "/html"},
-		{"css",  "/assets/css"},
-		{"js",   "/assets/js"},
-		{"jpg",  "/assets/images"},
-		{"jpeg", "/assets/images"},
-		{"png",  "/assets/images"},
-		{"gif",  "/assets/images"},
-		{"svg",  "/assets/images"},
-		{"ico",  "/assets/images"},
-		{"JSON", "/assets/JSONs"},
-		{"pdf",  "/assets/docs"},
-		{"zip",  "/assets/docs"},
-		{"mp3",  "/assets/audio"},
-		{"mp4",  "/assets/video"},
-		{"xml",  "/assets/docs"},
-		{"csv",  "/assets/docs"},
-		{"txt",  "/assets/docs"}
-	};
+    // Check if specific language is requested
 
-	// Check if specific language is requested
+    std::string language = httpRequest->hasHeader("Accept-Language") ? httpRequest->getHeader("Accept-Language") : "";
 
-	std::string language = httpRequest->hasHeader("Accept-Language") ?
-						   httpRequest->getHeader("Accept-Language") : "";
-
-
-
-	// TODO : Find better way to check for language, can't always add an 'or' statement for each new language
-	if (!startsWithLangPrefix(pathReceived)) {
-
-        if (pathReceived.size() == 1) {
+    // TODO : Find better way to check for language, can't always add an 'or' statement for each new language
+    if (!startsWithLangPrefix(pathReceived)) {
+        if (pathReceived.size() <= 1) {
             // if the size of the pathReceived is only 1 character long
             // then we can assume that the path is a language indicator for the index page
 
@@ -367,19 +334,19 @@ std::string Handler::buildPath(std::string &pathReceived, const std::string &Ext
             return serverData->getRoot() + "/html/en/index.html";
         }
 
-		if (language.find("de") != std::string::npos) {
-			contentRoot["html"] = "/html/de";
-		} else if (language.find("fr") != std::string::npos) {
-			contentRoot["html"] = "/html/fr";
-		} else {
-			contentRoot["html"] = "/html/en";
-		}
-	}else if( pathReceived.size() ==  4){
+        if (language.find("de") != std::string::npos) {
+            contentRoot["html"] = "/html/de";
+        } else if (language.find("fr") != std::string::npos) {
+            contentRoot["html"] = "/html/fr";
+        } else {
+            contentRoot["html"] = "/html/en";
+        }
+    } else if (pathReceived.size() == 4) {
         // if the size of the pathReceived has a language indicator and is only 4 characters long
         // then we can assume that the path is a language indicator for the index page
 
         pathReceived += "/index";
-    }else if(Extension != "html" && Extension != "htm"){
+    } else if (Extension != "html" && Extension != "htm") {
         // Remove the language prefix from the path
         pathReceived = pathReceived.substr(3);
     }
@@ -388,7 +355,7 @@ std::string Handler::buildPath(std::string &pathReceived, const std::string &Ext
         pathReceived += ".html";
     }
 
-	return contentRoot.count(Extension) ? serverData->getRoot() + contentRoot[Extension] + pathReceived : "";
+    return contentRoot.count(Extension) ? serverData->getRoot() + contentRoot[Extension] + pathReceived : "";
 }
 
 /**
@@ -397,27 +364,12 @@ std::string Handler::buildPath(std::string &pathReceived, const std::string &Ext
  * @return
  */
 std::string Handler::getContentType(const std::string &extension) {
+    std::map<std::string, std::string> contentTypes = {
+        {"html", "text/html"},      {"htm", "text/html"},    {"css", "text/css"},          {"js", "text/javascript"},
+        {"jpg", "image/jpeg"},      {"jpeg", "image/jpeg"},  {"png", "image/png"},         {"gif", "image/gif"},
+        {"svg", "image/svg+xml"},   {"ico", "image/x-icon"}, {"JSON", "application/JSON"}, {"pdf", "application/pdf"},
+        {"zip", "application/zip"}, {"mp3", "audio/mpeg"},   {"mp4", "video/mp4"},         {"xml", "application/xml"},
+        {"csv", "text/csv"},        {"txt", "text/plain"}};
 
-	std::map<std::string, std::string> contentTypes = {
-		{"html", "text/html"},
-		{"htm",  "text/html"},
-		{"css",  "text/css"},
-		{"js",   "text/javascript"},
-		{"jpg",  "image/jpeg"},
-		{"jpeg", "image/jpeg"},
-		{"png",  "image/png"},
-		{"gif",  "image/gif"},
-		{"svg",  "image/svg+xml"},
-		{"ico",  "image/x-icon"},
-		{"JSON", "application/JSON"},
-		{"pdf",  "application/pdf"},
-		{"zip",  "application/zip"},
-		{"mp3",  "audio/mpeg"},
-		{"mp4",  "video/mp4"},
-		{"xml",  "application/xml"},
-		{"csv",  "text/csv"},
-		{"txt",  "text/plain"}
-	};
-
-	return contentTypes.count(extension) ? contentTypes[extension] : "application/octet-stream";
+    return contentTypes.count(extension) ? contentTypes[extension] : "application/octet-stream";
 }
